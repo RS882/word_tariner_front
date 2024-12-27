00ed0a405659edd3b444ce055e9f095446767aeb
Rslan <russen333@gmail.com>
2024-12-27 08:47:37 +0100

    feat: add login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {LoginPageComponent} from "./page/login-page/login-page.component";
+import {RegistrationPageComponent} from "./page/registration-page/registration-page.component";
+import {LayoutComponent} from "./command-ui/layout/layout.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LayoutComponent, children:[
+      { path: '', component: LoginPageComponent },
+      { path: 'registration', component:RegistrationPageComponent }
+    ]}
+];
---
+++ src/app/constants/api.url.ts
@@ -0,0 +1,15 @@
+const BASE_API_URL: string = 'http://localhost:8095/api/';
+const VERSION_PREFIX: string = 'v1/';
+const AUTH_PREFIX: string = 'auth/';
+
+
+const getFullUrl = (path: string, prefix: string = ''): string =>
+  BASE_API_URL + VERSION_PREFIX + prefix + path;
+
+const getAuthUrl = (path: string): string => getFullUrl(path, AUTH_PREFIX);
+
+export const apiConstants = {
+  login: getAuthUrl('login'),
+
+}
+
